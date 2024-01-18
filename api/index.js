00922eda951982ed027ea6c0c58dbb3b24d8f668
yakoubi rayen <rayen08yako@gmail.com>
2024-01-18 20:07:31 +0100

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const port = 4004
-
+//leagueoflegends123 rayen08yako
 app.listen(port , ()=> {
     console.log('listenning to port : 4004');
 })
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>console.log('connected to the database'))
+.catch((err) => console.error(err))
