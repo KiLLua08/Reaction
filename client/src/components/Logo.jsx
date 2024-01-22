637c92c36584d9060073b1f24b675450743a524b
yakoubi rayen <rayen08yako@gmail.com>
2024-01-22 21:17:56 +0100

    sign up/in UI

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Logo() {
   return (
-    <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-        <span className=''>Talky</span>
-        Walky
+    <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white px-2 py-1 bg-gradient-to-r from-indigo-950 via-purple-500 to-pink-950 rounded-lg text-white '>
+        <span className=''>Skills</span>
+        print
     </Link>
   )
 }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import killua from '../assets/killua.png'
+import straw from '../assets/straw.png'
+import luffy from '../assets/luffy.png'
+import {Link, } from 'react-router-dom'
+import { Button, TextInput, Spinner, Label, Alert} from 'flowbite-react';
 
-function Signin() {
+function Signup() {
+    const [loading, setLoading] = useState(true)
+
   return (
-    <div className=''>
-      SI
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 bg-gradient-to-r from-indigo-950 via-purple-500 to-pink-950 rounded-lg p-5 items-center justify-center'>
+        {/* left */}
+        <div className='flex-1 items-center justify-center'>
+          <Link to='/' className='self-center whitespace-nowrap text-xl sm:text-2xl font-semibold dark:text-white px-4 py-2 bg-gradient-to-r from-indigo-950 via-purple-500 to-pink-950 rounded-lg text-white'>
+            SkillsPrint
+          </Link>
+        </div>
+        {/* right */}
+
+        <div className='flex-1 bg-white p-5 rounded'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='KiLLuLa'
+                id='name'
+              />
+            </div>
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@something.com'
+                id='email'
+              />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='**********'
+                id='password'
+              />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+            >
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Dont Have an account?</span>
+            <Link to='/signup' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+            <Alert className='mt-5' color='failure'>
+              errorMessage
+            </Alert>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Signin
+export default Signup
